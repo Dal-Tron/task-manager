70b39a274498801c448663717d9bce1dc14cb877
inboxDallas <inboxDallas@gmail.com>
2024-08-23 15:58:02 -0700

    feat: add some new animations to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,13 +13,30 @@ const config: Config = {
           '0%': { transform: 'translateX(100%)' },
           '100%': { transform: 'translateX(0)' },
         },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-1deg)' },
+          '50%': { transform: 'rotate(1deg)' },
+        },
+        pulseWiggle: {
+          '0%, 100%': {
+            transform: 'rotate(-1deg)',
+            opacity: '1',
+          },
+          '50%': {
+            transform: 'rotate(1deg)',
+            opacity: '0.5',
+          },
+        },
       },
       animation: {
         slideOutLeft: 'slideOutLeft 0.5s forwards',
         slideInRight: 'slideInRight 0.5s forwards',
+        wiggle: 'wiggle 0.3s ease-in-out infinite',
+        pulseWiggle: 'pulseWiggle 1s ease-in-out infinite',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
