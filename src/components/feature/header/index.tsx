847016cdfaa1fcaa62914214e1180f17640e03ca
inboxDallas <inboxDallas@gmail.com>
2024-08-23 15:04:31 -0700

    feat: adding animation to the header

--- src/components/feature/header/index.tsx
+++ src/components/feature/header/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { Bars3Icon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import { usePathname, useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 import { ButtonIcon } from '@/components/base/button-icon';
@@ -15,8 +17,25 @@ const navigation = [
   { name: 'Company', href: '#' },
 ];
 
-export const Header: React.FC = () => {
+export const Header: React.FC<{ onSignIn?: () => void }> = ({ onSignIn }) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleSignIn = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      onSignIn?.();
+    }, 500);
+  };
+
+  const handleGoHome = () => {
+    router.push('/');
+  };
+
+  const isSignInPage = pathname === '/sign-in';
 
   return (
     <header className="relative z-20">
@@ -24,25 +43,37 @@ export const Header: React.FC = () => {
         aria-label="Global"
         className="flex items-center justify-between p-6 lg:px-8"
       >
-        <div className="flex lg:flex-1">
+        <div className="flex lg:flex-1" onClick={handleGoHome}>
           <Logo />
         </div>
-        <div className="flex justify-center flex-1 lg:flex-none">
-          <NavigationLinks navigation={navigation} />
-        </div>
-        <div className="flex lg:flex-1 lg:justify-end">
+        {!isSignInPage && (
+          <div
+            className={clsx(
+              'flex justify-center flex-1 lg:flex-none',
+              isAnimating && 'opacity-0 transition-opacity duration-500'
+            )}
+          >
+            <NavigationLinks navigation={navigation} />
+          </div>
+        )}
+        <div className="flex lg:flex-1 lg:justify-end items-center">
           <ButtonIcon
             className="lg:hidden"
             srText="Open mobile menu"
             icon={Bars3Icon}
             onClick={() => setMobileMenuOpen(true)}
           />
-          <a
-            href="#"
-            className="hidden lg:inline-block text-sm font-semibold leading-6 text-gray-900 ml-6"
-          >
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
+          {!isSignInPage && (
+            <div
+              onClick={handleSignIn}
+              className={clsx(
+                'hidden lg:inline-block text-sm font-semibold leading-6 text-gray-900 ml-6 cursor-pointer',
+                isAnimating && 'opacity-0 transition-opacity duration-500'
+              )}
+            >
+              Sign in <span aria-hidden="true">&rarr;</span>
+            </div>
+          )}
         </div>
       </nav>
       <MobileMenu
