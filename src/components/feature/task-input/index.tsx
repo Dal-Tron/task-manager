84663a2f8bdfd5011ab54cb7837b120fece0fcc3
inboxDallas <inboxDallas@gmail.com>
2024-08-23 16:41:15 -0700

    feat: disable task inputs on main page

--- src/components/feature/task-input/index.tsx
+++ src/components/feature/task-input/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/base/button';
 import { Input } from '@/components/base/input';
 import { ResizableTextarea } from '@/components/base/resize-textarea';
 
@@ -23,19 +24,21 @@ export const TaskInput: React.FC<TaskInputProps> = ({
         onChange={(e) => onInputChange(e.target.value)}
         placeholder="Develop better habits"
         className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl w-full mb-4 !-pl-5"
+        disabled={true}
       />
       <ResizableTextarea
         value={descriptionValue}
         onChange={onDescriptionChange}
         placeholder="Establish a routine for daily exercises"
         className="mt-2 text-lg leading-8 text-gray-600 w-full"
+        disabled={true}
       />
-      <button
+      <Button
         onClick={onSaveTask}
-        className="mt-6 bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600"
-      >
-        Save Task
-      </button>
+        className="mt-6 bg-blue-500 text-white font-semibold py-2 px-4 rounded-md"
+        disabled={true}
+        text="Save Task"
+      />
     </div>
   );
 };
