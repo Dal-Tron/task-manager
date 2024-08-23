aaf3f33a083430bd4abfc272a88929db00ec7e5d
inboxDallas <inboxDallas@gmail.com>
2024-08-23 15:00:17 -0700

    style: switching the style of the button icon

--- src/components/base/button-icon/index.tsx
+++ src/components/base/button-icon/index.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 
 interface ButtonIconProps {
-  className: string;
+  className?: string;
   icon: React.ElementType;
   onClick: () => void;
   srText: string;
@@ -17,7 +17,7 @@ export const ButtonIcon: React.FC<ButtonIconProps> = ({
     type="button"
     onClick={onClick}
     className={clsx(
-      '-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700',
+      'inline-flex items-center justify-center rounded-md text-gray-700',
       className
     )}
   >
