9769f52821b29906a424f1997300332d36f71bbb
inboxDallas <inboxDallas@gmail.com>
2024-08-23 14:59:07 -0700

    feat: adding animation for sliding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideOutLeft: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        slideInRight: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        slideOutLeft: 'slideOutLeft 0.5s forwards',
+        slideInRight: 'slideInRight 0.5s forwards',
+      },
+    },
   },
   plugins: [],
 };
