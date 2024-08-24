1298c93b959818de5fcb49a3fc96e65fcc69eb01
inboxDallas <inboxDallas@gmail.com>
2024-08-23 21:22:58 -0700

    feat: adding classname to the skeleton card

--- src/components/base/skeleton/SkeletonTaskCard.tsx
+++ src/components/base/skeleton/SkeletonTaskCard.tsx
@@ -1,3 +1,18 @@
-export const SkeletonTaskCard: React.FC = () => (
-  <div className="animate-pulseWiggle bg-gray-300 h-48 rounded-lg" />
+import clsx from 'clsx';
+
+interface SkeletonTaskCardProps {
+  className?: string;
+}
+
+export const SkeletonTaskCard: React.FC<SkeletonTaskCardProps> = ({
+  className,
+}) => (
+  <div
+    className={clsx(
+      'relative animate-pulseWiggle bg-gray-300 h-48 rounded-lg flex items-center justify-center',
+      className
+    )}
+  >
+    <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-500" />
+  </div>
 );
