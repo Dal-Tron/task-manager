e1902de377753e296aa016e3ccd77ce463bfa638
inboxDallas <inboxDallas@gmail.com>
2024-08-24 20:19:21 -0700

    chore: refactor dashboard content component

--- src/components/feature/dashboard-content/index.tsx
+++ src/components/feature/dashboard-content/index.tsx
@@ -3,87 +3,44 @@
 import { useEffect, useState } from 'react';
 
 import { HorizontalLine } from '@/components/base/horizontal-line';
-import { SkeletonTaskCard } from '@/components/base/skeleton/SkeletonTaskCard';
-import { TaskCard } from '@/components/feature/task-card';
 import { TaskInput } from '@/components/feature/task-input';
 import TaskService from '@/services/tasks';
+import { ITask } from '@/types/task';
 
-import { SubtaskSection } from './components/SubtaskSection';
+import { TaskList } from './components/TaskList';
+import { useTaskManager } from './hooks/useTaskManager';
 
-interface Task {
-  id: number;
-  title: string;
-  description: string;
-  start_date: string;
-  due_date: string;
-  completion: number;
-  user_id: string;
+interface DashboardContentProps {
+  params?: { task_id?: string };
 }
 
-export const DashboardContent = () => {
-  const [inputValue, setInputValue] = useState('');
-  const [descriptionValue, setDescriptionValue] = useState('');
-  const [subtasks, setSubtasks] = useState<Task[]>([]);
-  const [loadingSubtasks, setLoadingSubtasks] = useState(false);
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [loadingTasks, setLoadingTasks] = useState(true);
-  const [showSubtasks, setShowSubtasks] = useState(false);
-
-  const handleSaveTask = async () => {
-    const task = await TaskService.createTask({
-      title: inputValue,
-      description: descriptionValue,
-      start_date: new Date().toISOString(),
-      due_date: new Date().toISOString(),
-      completion: 0,
-    });
-  };
-
-  const handleDeleteTask = async (id: number) => {
-    const success = await TaskService.deleteTask(id);
-    if (success) {
-      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
-    } else {
-      console.error('Failed to delete the task');
-    }
-  };
-
-  const handleGetSubtasks = async () => {
-    setShowSubtasks(true);
-    setLoadingSubtasks(true);
-    setSubtasks([]);
-
-    try {
-      const response = await fetch('/api/generate-subtasks', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          title: inputValue,
-          description: descriptionValue,
-        }),
-      });
-
-      const data = await response.json();
-      setSubtasks(data.subtasks);
-    } catch (error) {
-      console.error('Error saving task or generating subtasks:', error);
-    } finally {
-      setLoadingSubtasks(false);
-    }
-  };
+export default function DashboardContent({ params }: DashboardContentProps) {
+  const [task, setTask] = useState<ITask | null>(null);
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      setLoadingTasks(true);
-      const fetchedTasks = await TaskService.getTasks();
-      setTasks(fetchedTasks || []);
-      setLoadingTasks(false);
+    const fetchTask = async () => {
+      if (params?.task_id) {
+        const fetchedTask = await TaskService.getTaskById(
+          Number(params.task_id)
+        );
+        setTask(fetchedTask);
+      }
     };
 
-    fetchTasks();
-  }, []);
+    fetchTask();
+  }, [params?.task_id]);
+
+  const {
+    inputValue,
+    setInputValue,
+    descriptionValue,
+    setDescriptionValue,
+    tasks,
+    loadingTasks,
+    handleSaveTask,
+    handleDeleteTask,
+    handleGetSubtasks,
+  } = useTaskManager(task);
 
   return (
     <div className="pb-14 sm:py-20">
@@ -99,45 +56,14 @@ export const DashboardContent = () => {
           />
         </div>
 
-        {showSubtasks && (
-          <SubtaskSection
-            subtasks={subtasks}
-            loadingSubtasks={loadingSubtasks}
-          />
-        )}
-
         <HorizontalLine text="Current Tasks" pulse={false} />
 
-        <div className="mt-8">
-          <div className="mx-auto grid w-full max-w-full grid-cols-1 gap-x-4 gap-y-8 sm:px-6 md:grid-cols-2 lg:grid-cols-3 lg:px-0">
-            {loadingTasks ? (
-              <>
-                <SkeletonTaskCard />
-                <SkeletonTaskCard />
-                <SkeletonTaskCard />
-                <SkeletonTaskCard />
-                <SkeletonTaskCard />
-                <SkeletonTaskCard />
-              </>
-            ) : (
-              tasks.map((task) => (
-                <TaskCard
-                  key={task.id}
-                  id={task.id}
-                  title={task.title}
-                  description={task.description}
-                  start_date={task.start_date}
-                  due_date={task.due_date}
-                  updated_at={new Date().toISOString()}
-                  completion={task.completion}
-                  className="min-h-[12rem]"
-                  onDelete={handleDeleteTask}
-                />
-              ))
-            )}
-          </div>
-        </div>
+        <TaskList
+          tasks={tasks}
+          loadingTasks={loadingTasks}
+          onDelete={handleDeleteTask}
+        />
       </div>
     </div>
   );
-};
+}
