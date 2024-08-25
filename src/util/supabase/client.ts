c34f0147a60f26daf10c7aae85b55c4e74d076b6
inboxDallas <inboxDallas@gmail.com>
2024-08-25 13:19:09 -0700

    chore: small changes to the supabase function to conset

--- src/util/supabase/client.ts
+++ src/util/supabase/client.ts
@@ -1,10 +1,11 @@
 'use client';
 
 import { createBrowserClient } from '@supabase/ssr';
+import { SupabaseClient } from '@supabase/supabase-js';
 
-export function createClient() {
+export const createClient = (): SupabaseClient => {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
-}
+};
