c1e16f39ba74e6056b5fa6f89cb2e7a6778e40e3
inboxDallas <inboxDallas@gmail.com>
2024-08-25 16:32:27 -0700

    feat: move task list into feature components

--- src/components/feature/task-list/index.tsx
+++ src/components/feature/task-list/index.tsx
@@ -1,22 +1,38 @@
 'use client';
 
+import clsx from 'clsx';
+import { useRouter } from 'next/navigation';
+
 import { SkeletonTaskCard } from '@/components/base/skeleton';
+import { CreateTaskCard } from '@/components/feature/create-task-card';
 import { TaskCard } from '@/components/feature/task-card';
 import { ITask } from '@/types/task';
-
-import { CreateTaskCard } from '../../create-task';
 
 interface TaskListProps {
   tasks: ITask[];
   loadingTasks: boolean;
   onDelete: (id: number) => void;
+  currentTaskId?: string;
+  isMain?: boolean;
 }
 
 export const TaskList: React.FC<TaskListProps> = ({
   tasks,
   loadingTasks,
   onDelete,
+  currentTaskId,
+  isMain = false,
 }) => {
+  const router = useRouter();
+
+  const handleTaskClick = (id: number) => {
+    if (isMain) {
+      router.push(`/dashboard/${id}`);
+    } else {
+      console.log('Clicked suggestion');
+    }
+  };
+
   return (
     <div className="mt-8">
       <div className="mx-auto grid w-full max-w-full grid-cols-1 gap-x-4 gap-y-8 sm:px-6 md:grid-cols-2 lg:grid-cols-3 lg:px-0">
@@ -37,8 +53,13 @@ export const TaskList: React.FC<TaskListProps> = ({
               due_date={task.due_date}
               updated_at={task.updated_at}
               completion={task.completion}
-              className="min-h-[12rem]"
+              className={clsx(
+                'min-h-[12rem]',
+                task.id.toString() === currentTaskId &&
+                  'border-2 border-blue-500'
+              )}
               onDelete={onDelete}
+              onClick={() => handleTaskClick(task.id)}
             />
           ))
         )}
