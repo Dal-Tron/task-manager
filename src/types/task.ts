dd2740f0cf1012259abff8e08aea841e4753b9d1
inboxDallas <inboxDallas@gmail.com>
2024-08-24 20:22:09 -0700

    feat: add interface for task

---
+++ src/types/task.ts
@@ -0,0 +1,10 @@
+export interface ITask {
+  id: number;
+  title: string;
+  description: string;
+  start_date: string;
+  due_date: string;
+  completion: number;
+  user_id: string;
+  updated_at: string;
+}
