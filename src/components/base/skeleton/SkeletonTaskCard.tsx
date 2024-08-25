8bc8eacd0ef86b9b2fad2e3b8903a90aa448ddc0
inboxDallas <inboxDallas@gmail.com>
2024-08-24 19:18:38 -0700

    feat: add a ghost variant to the skeleton card

--- src/components/base/skeleton/SkeletonTaskCard.tsx
+++ src/components/base/skeleton/SkeletonTaskCard.tsx
@@ -2,17 +2,24 @@ import clsx from 'clsx';
 
 interface SkeletonTaskCardProps {
   className?: string;
+  variant?: 'skeleton' | 'ghost';
 }
 
 export const SkeletonTaskCard: React.FC<SkeletonTaskCardProps> = ({
   className,
+  variant = 'skeleton',
 }) => (
   <div
     className={clsx(
-      'relative animate-pulseWiggle bg-gray-300 h-48 rounded-lg flex items-center justify-center',
+      'relative h-48 rounded-lg flex items-center justify-center',
+      variant === 'skeleton' && 'animate-pulseWiggle bg-gray-300',
+      variant === 'ghost' &&
+        'border-2 border-dotted border-gray-500 bg-transparent',
       className
     )}
   >
-    <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-500" />
+    {variant === 'skeleton' && (
+      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-gray-500" />
+    )}
   </div>
 );
